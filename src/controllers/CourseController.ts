88a828baf156603459ccb77b099c36f1b9624047
Imon <imon@flarie.com>
2022-10-03 18:50:56 +0600

    Delete Course Successfully

--- src/controllers/CourseController.ts
+++ src/controllers/CourseController.ts
@@ -2,7 +2,7 @@ import { JwtAuthGuard } from '@/auth/JwtAuthGuard';
 import { CreateCourseDto } from '@/dto/CreateCourseDto';
 import { Course } from '@/entities/Course';
 import { CourseService } from '@/services/CourseService';
-import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('course')
@@ -24,8 +24,8 @@ export class CourseController {
   @Post()
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth('token')
-  @ApiOperation({ summary: 'Create an Course' })
-  @ApiResponse({ status: 201, description: 'Return an Course' })
+  @ApiOperation({ summary: 'Create a Course' })
+  @ApiResponse({ status: 201, description: 'Return a Course' })
   @ApiResponse({ status: 400, description: 'BAD REQUEST' })
   @ApiResponse({ status: 401, description: 'UNAUTHORIZED' })
   @ApiResponse({ status: 403, description: 'FORBIDDEN' })
@@ -36,13 +36,24 @@ export class CourseController {
   @Patch(':id')
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth('token')
-  @ApiOperation({ summary: 'Update an Course' })
-  @ApiResponse({ status: 200, description: 'Return an Course' })
+  @ApiOperation({ summary: 'Update a Course' })
+  @ApiResponse({ status: 200, description: 'Return a Course' })
   @ApiResponse({ status: 400, description: 'BAD REQUEST' })
   @ApiResponse({ status: 401, description: 'UNAUTHORIZED' })
   @ApiResponse({ status: 403, description: 'FORBIDDEN' })
   updateCourse(@Param('id') id: string, @Body() createCourseDto: CreateCourseDto): Promise<Course> {
-    console.log('createCourseDto-----', createCourseDto);
     return this.courseService.updateCourse(id, createCourseDto);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('token')
+  @ApiOperation({ summary: 'Delete an Course' })
+  @ApiResponse({ status: 200, description: 'Delete Successfully' })
+  @ApiResponse({ status: 400, description: 'BAD REQUEST' })
+  @ApiResponse({ status: 401, description: 'UNAUTHORIZED' })
+  @ApiResponse({ status: 403, description: 'FORBIDDEN' })
+  deleteCourse(@Param('id') id: string): Promise<void> {
+    return this.courseService.deleteCourse(id);
   }
 }
