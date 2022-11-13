0408f9b4c8f3163962554da2f043d61c64377661
Imon <imon@flarie.com>
2022-11-13 11:59:35 +0600

    Room list show, add room, delete room

--- front-end-app/src/components/course/CourseForm.tsx
+++ front-end-app/src/components/course/CourseForm.tsx
@@ -80,24 +80,6 @@ const CourseForm: FC<Props> = ({
           }
           options={SemesterConstant}
         />
-        {/* <Select
-          label="Semester"
-          value={course.semester?.toString()}
-          onChange={(value: string) => onChangeSelect(value)}
-        >
-          <Option value="1">1</Option>
-          <Option value="2">2</Option>
-          <Option value="3">3</Option>
-          <Option value="4">4</Option>
-          <Option value="5">5</Option>
-          <Option value="6">6</Option>
-          <Option value="7">7</Option>
-          <Option value="8">8</Option>
-          <Option value="9">9</Option>
-          <Option value="10">10</Option>
-          <Option value="11">11</Option>
-          <Option value="12">12</Option>
-        </Select> */}
         <Switch
           label="Auto assign"
           defaultChecked={course.isAutoAssign}
---
+++ front-end-app/src/interfaces/Room.ts
@@ -0,0 +1,7 @@
+interface IRoom {
+  id: string;
+  number: string;
+  capacity: number;
+  isAutoAssign: boolean;
+}
+export default IRoom;
